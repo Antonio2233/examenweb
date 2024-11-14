48db5246e65d4ac485994ac166129dc116534a44
Antonio2233 <antoniosalvadorzafra@gmail.com>
2024-11-14 17:11:31 +0100

    vaciado

--- prueba/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from routes import usuario_route,evento_route
-
-app = FastAPI()
-
-app.include_router(usuario_route.router, prefix="/usuarios")
-app.include_router(evento_route.router, prefix="/eventos")
